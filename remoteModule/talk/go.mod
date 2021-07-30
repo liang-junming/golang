dd5e69b71e4e44aff3f791444f3fa7cac02ee096
liangjunming <allenprivate@163.com>
2021-07-30 14:32:16 +0800

    已验证module名和路径名不同会发生错误，删除talk

--- remoteModule/talk/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/liang-junming/golang/talk
-
-go 1.16
--- remoteModule/talk/talk.go
+++
@@ -1,8 +0,0 @@
-package talk
-
-import "fmt"
-
-func Talk(word string) string {
-    message := fmt.Sprintf("talk %v", word)
-    return message
-}
